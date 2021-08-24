998a195a38f3e5203e596295a5bb80e5a97074fd
Rana ahmed <shakilahmedrana5@gmail.com>
2021-08-24 11:30:07 +0600

    root api created

--- index.js
+++ index.js
@@ -11,7 +11,9 @@ app.use(bodyParser.json())
 app.use(cors())
 const port = 5000;
 
-
+app.get('/',(req,res)=>{
+  res.send('hello from db its working')
+})
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 client.connect(err => {
   const productCollections = client.db("emaJohnStore").collection("products");
